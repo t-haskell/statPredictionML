fc7c1099a0e7055c6c1db4b735d52f45b963a146
Tommy <tommyhaskell3@gmail.com>
2023-08-02 16:28:21 -0400

    beginnings of tensorFlow

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import dbConnect
 import matplotlib.pyplot as plt
+import tensorflow as tf
 import seaborn as sns
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
@@ -17,18 +18,23 @@ if __name__ == '__main__':
     print_hi('PyCharm')
     frame = dbConnect.dataRetrieve()
     print(frame.info())
+    print("Before drop")
 
     # Drop the "First Name" and "Last Name" columns
-    frame.drop(columns=['First Name', 'Last Name', 'Position'], inplace=True)
-    frame.dropna()
-
-    # Scatter plot for Points vs. Shots
-    plt.figure(figsize=(8, 6))
-    plt.scatter(frame['Points'], frame['Time On Field'], color='orange')
-    plt.title('Time on Field vs. Points')
-    plt.xlabel('Points')
-    plt.ylabel('Time on Field')
-    plt.show()
+    frame.drop(columns=['First Name', 'Last Name', 'Position', 'Short Handed Goals Against'], inplace=True)
+    print(frame.info())
+    #Convert data types to float64
+    frame = frame.astype(float)
+    print(frame.head())
+    print(frame.info())
+    #
+    # # Scatter plot for Points vs. Shots
+    # plt.figure(figsize=(8, 6))
+    # plt.scatter(frame['Points'], frame['Time On Field'], color='orange')
+    # plt.title('Time on Field vs. Points')
+    # plt.xlabel('Points')
+    # plt.ylabel('Time on Field')
+    # plt.show()
 
     #
     # # Heatmap for correlation matrix
@@ -36,6 +42,12 @@ if __name__ == '__main__':
     # sns.heatmap(frame.corr(), annot=True, cmap='coolwarm', fmt='.2f')
     # plt.title('Correlation Matrix')
     # plt.show()
+
+    ####### TensorFlow Manipulation ######
+    playerArr = frame.to_numpy()
+    pllTensor = tf.constant(playerArr, dtype=tf.float64)
+
+    print(pllTensor)
 
 
 
