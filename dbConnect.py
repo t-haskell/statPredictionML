91717b5a64547b9d18f97b623551b6fcd66166c3
Tommy <tommyhaskell3@gmail.com>
2023-08-09 18:22:41 -0400

    Normalized column names, lowercase and no spaces

--- dbConnect.py
+++ dbConnect.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 
 
-def dataRetrieve(year):
-    #Credentials to the database full of stats
+def dataRetrieve():
+    # Credentials to the database full of stats
     db_config = {
         'host': 'plldatabase-1.cpzseqbr5kxy.us-east-1.rds.amazonaws.com',
         'port': 3306,
@@ -22,9 +22,24 @@ def dataRetrieve(year):
             # Create a cursor to execute SQL queries
             cursor = connection.cursor()
 
-            # Execute the SELECT statement to fetch all players with jersey number 8
-            jersey_number = 8
-            select_query = f"SELECT * FROM stats2023 WHERE Jersey={jersey_number};"
+            # Execute the ALTER statement to normalize all column names
+            query = "SELECT table_name, column_name FROM information_schema.columns WHERE table_schema = %s;"
+            cursor.execute(query, (db_config['database'],))
+            for table_name, column_name in cursor.fetchall():
+                new_column_name = column_name.lower().replace(' ', '_')
+                if '_' in new_column_name:
+                    column_name = f"`{column_name}`"
+                alter_query = f"ALTER TABLE {table_name} RENAME COLUMN {column_name} TO {new_column_name};"
+                # Execute the ALTER query here
+                try:
+                    cursor.execute(alter_query)
+                    connection.commit()
+                    print(f"Column name changed in table {table_name}: {column_name} -> {new_column_name}")
+                except mysql.connector.Error as err:
+                    print(f"Error altering table {table_name} - {err}")
+            cursor.close()
+
+            # Gathers all player stats from a given year
             selectALL_query = f"SELECT * FROM {year};"
 
 
@@ -50,3 +65,5 @@ def dataRetrieve(year):
         if connection and connection.is_connected():
             connection.close()
             print("Connection closed.")
+
+dataRetrieve()
