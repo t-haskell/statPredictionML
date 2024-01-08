6716646887529afb13032513803356ec5672ae51
Tommy <tommyhaskell3@gmail.com>
2024-01-07 19:14:12 -0500

    Preprocessing and feature selection on stat dataframes

--- revamp.py
+++ revamp.py
@@ -33,4 +33,16 @@ print("2018: ", f18.head(), f18.shape)
 years = [f23, f22, f21, f20, f19, f18]
 
 ## Preprocessing dataframes ##
+processedVector = []
+for i in years:
+    # Feature selection for point prediction
+    i = i[['Position', 'Points', 'Games Played', 'Shots', 'Shots On Goal', '2pt Shots', '2pt Shots On Goal', 'Groundballs', 'Caused Turnovers']]
+    i.replace(['FO', 'M', 'SSDM', 'LSM', 'A', 'D', 'G'], [0,1,2,3,4,5,6], inplace=True)
+    processedVector.append(i)
+    
+for i in processedVector:
+    print(i.head(), i.shape)
+
+
+
 
