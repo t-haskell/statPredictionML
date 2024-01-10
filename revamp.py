56371dcda5c839f9c222caa1cf7e8827227fa0bc
Tommy <tommyhaskell3@gmail.com>
2024-01-10 02:20:24 -0500

    Preprocessing PLL statistical data

--- revamp.py
+++ revamp.py
@@ -16,6 +16,7 @@ import sklearn
 from sklearn import preprocessing
 from sklearn import metrics
 
+
 import xgboost
 
 f23 = pd.read_csv('stats2023.csv')
@@ -24,24 +25,30 @@ f21 = pd.read_csv('stats2021.csv')
 f20 = pd.read_csv('stats2020.csv')
 f19 = pd.read_csv('stats2019.csv')
 f18 = pd.read_csv('stats2018.csv')
-print("2023: ", f23.head(), f23.shape)
-print("2022: ", f22.head(), f22.shape)
-print("2021: ", f21.head(), f21.shape)
-print("2020: ", f20.head(), f20.shape)
-print("2019: ", f19.head(), f19.shape)
-print("2018: ", f18.head(), f18.shape)
+print("2023: ", f23.describe(), f23.shape)
+print("2022: ", f22.describe(), f22.shape)
+# print("2021: ", f21.head(), f21.shape)
+# print("2020: ", f20.head(), f20.shape)
+# print("2019: ", f19.head(), f19.shape)
+# print("2018: ", f18.head(), f18.shape)
 years = [f23, f22, f21, f20, f19, f18]
 
+
 ## Preprocessing dataframes ##
-processedVector = []
-for i in years:
-    # Feature selection for point prediction
-    i = i[['Position', 'Points', 'Games Played', 'Shots', 'Shots On Goal', '2pt Shots', '2pt Shots On Goal', 'Groundballs', 'Caused Turnovers']]
-    i.replace(['FO', 'M', 'SSDM', 'LSM', 'A', 'D', 'G'], [0,1,2,3,4,5,6], inplace=True)
-    processedVector.append(i)
-    
-for i in processedVector:
-    print(i.head(), i.shape)
+merged_df = pd.concat([f23, f22, f21, f20, f19, f18], axis=0, join='outer')
+# selecting independent variables (Feature Selection)
+X = merged_df[['Position', 'Games Played', 'Shots', 'Shots On Goal', '2pt Shots', '2pt Shots On Goal', 'Groundballs', 'Caused Turnovers']].values
+print(X[0:8])
+le_pos = preprocessing.LabelEncoder()
+le_pos.fit(['FO', 'M', 'SSDM', 'LSM', 'A', 'D', 'G','nan'])
+X[:,0] = le_pos.transform(X[:,0])
+X = X.astype(float)
+print(X.shape)
+print(X[0:8])
+# selecting dependent variable (target variable)
+y = merged_df["Points"]
+print(y.shape)
+print(y[0:8])
 
 
 
